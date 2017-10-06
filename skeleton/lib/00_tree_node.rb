7140cb317a80829a6b9b9a3cb227af55fdcff2d4
TV <trungvuh@gmail.com>
2017-10-06 11:01:41 -0700

    Initialize parent node

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,25 @@
+class PolyTreeNode
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(parent)
+    @parent = parent
+    parent.children << self
+  end
+
+end
